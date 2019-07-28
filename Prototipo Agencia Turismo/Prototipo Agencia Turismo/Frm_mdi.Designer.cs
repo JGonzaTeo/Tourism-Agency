06efbe347c4c35a8b73b0ba58da4b55ff049d85b
Unknown <imogollonh@miumg.edu.gt>
2019-07-28 12:41:46 -0600

    Cambio mdi

--- Prototipo Agencia Turismo/Prototipo Agencia Turismo/Frm_mdi.Designer.cs
+++ Prototipo Agencia Turismo/Prototipo Agencia Turismo/Frm_mdi.Designer.cs
@@ -44,13 +44,14 @@
             this.departamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bonosYDescuentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guiaTuristicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contabilidadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nóminasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cerrarSesiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.guiaTuristicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.empleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MenStrip_menuPrincipal.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -70,7 +71,7 @@
             this.cerrarSesiónToolStripMenuItem});
             this.MenStrip_menuPrincipal.Location = new System.Drawing.Point(0, 0);
             this.MenStrip_menuPrincipal.Name = "MenStrip_menuPrincipal";
-            this.MenStrip_menuPrincipal.Size = new System.Drawing.Size(1182, 38);
+            this.MenStrip_menuPrincipal.Size = new System.Drawing.Size(1182, 35);
             this.MenStrip_menuPrincipal.TabIndex = 1;
             this.MenStrip_menuPrincipal.Text = "menuStrip1";
             // 
@@ -78,21 +79,21 @@
             // 
             this.reservacionesToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.reservacionesToolStripMenuItem.Name = "reservacionesToolStripMenuItem";
-            this.reservacionesToolStripMenuItem.Size = new System.Drawing.Size(145, 34);
+            this.reservacionesToolStripMenuItem.Size = new System.Drawing.Size(127, 31);
             this.reservacionesToolStripMenuItem.Text = "Reservaciones";
             // 
             // guíasTurísticasToolStripMenuItem
             // 
             this.guíasTurísticasToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guíasTurísticasToolStripMenuItem.Name = "guíasTurísticasToolStripMenuItem";
-            this.guíasTurísticasToolStripMenuItem.Size = new System.Drawing.Size(155, 34);
+            this.guíasTurísticasToolStripMenuItem.Size = new System.Drawing.Size(133, 31);
             this.guíasTurísticasToolStripMenuItem.Text = "Guías turísticas";
             // 
             // planesDeViajeToolStripMenuItem
             // 
             this.planesDeViajeToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.planesDeViajeToolStripMenuItem.Name = "planesDeViajeToolStripMenuItem";
-            this.planesDeViajeToolStripMenuItem.Size = new System.Drawing.Size(149, 34);
+            this.planesDeViajeToolStripMenuItem.Size = new System.Drawing.Size(131, 31);
             this.planesDeViajeToolStripMenuItem.Text = "Planes de viaje";
             // 
             // cotizacionesToolStripMenuItem
@@ -100,7 +101,7 @@
             this.cotizacionesToolStripMenuItem.BackColor = System.Drawing.Color.White;
             this.cotizacionesToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cotizacionesToolStripMenuItem.Name = "cotizacionesToolStripMenuItem";
-            this.cotizacionesToolStripMenuItem.Size = new System.Drawing.Size(120, 34);
+            this.cotizacionesToolStripMenuItem.Size = new System.Drawing.Size(105, 31);
             this.cotizacionesToolStripMenuItem.Text = "Facturación";
             this.cotizacionesToolStripMenuItem.Click += new System.EventHandler(this.cotizacionesToolStripMenuItem_Click);
             // 
@@ -113,28 +114,28 @@
             this.perfilesToolStripMenuItem});
             this.seguridadToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
-            this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(105, 34);
+            this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(94, 31);
             this.seguridadToolStripMenuItem.Text = "Seguridad";
             // 
             // bitácoraToolStripMenuItem
             // 
             this.bitácoraToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bitácoraToolStripMenuItem.Name = "bitácoraToolStripMenuItem";
-            this.bitácoraToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.bitácoraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.bitácoraToolStripMenuItem.Text = "Bitácora";
             this.bitácoraToolStripMenuItem.Click += new System.EventHandler(this.bitácoraToolStripMenuItem_Click);
             // 
             // usuariosToolStripMenuItem
             // 
             this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
-            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.usuariosToolStripMenuItem.Text = "Usuarios";
             this.usuariosToolStripMenuItem.Click += new System.EventHandler(this.usuariosToolStripMenuItem_Click);
             // 
             // perfilesToolStripMenuItem
             // 
             this.perfilesToolStripMenuItem.Name = "perfilesToolStripMenuItem";
-            this.perfilesToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.perfilesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.perfilesToolStripMenuItem.Text = "Perfiles";
             this.perfilesToolStripMenuItem.Click += new System.EventHandler(this.perfilesToolStripMenuItem_Click);
             // 
@@ -149,17 +150,18 @@
             this.clienteToolStripMenuItem,
             this.bonosYDescuentosToolStripMenuItem,
             this.guiaTuristicoToolStripMenuItem,
-            this.empleadoToolStripMenuItem});
+            this.empleadoToolStripMenuItem,
+            this.menuToolStripMenuItem});
             this.mantenimientosToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
-            this.mantenimientosToolStripMenuItem.Size = new System.Drawing.Size(143, 34);
+            this.mantenimientosToolStripMenuItem.Size = new System.Drawing.Size(125, 31);
             this.mantenimientosToolStripMenuItem.Text = "Mantenimiento";
             // 
             // transporteToolStripMenuItem
             // 
             this.transporteToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.transporteToolStripMenuItem.Name = "transporteToolStripMenuItem";
-            this.transporteToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
+            this.transporteToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
             this.transporteToolStripMenuItem.Text = "Transporte";
             this.transporteToolStripMenuItem.Click += new System.EventHandler(this.transporteToolStripMenuItem_Click);
             // 
@@ -167,7 +169,7 @@
             // 
             this.restauranteToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.restauranteToolStripMenuItem.Name = "restauranteToolStripMenuItem";
-            this.restauranteToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
+            this.restauranteToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
             this.restauranteToolStripMenuItem.Text = "Restaurante";
             this.restauranteToolStripMenuItem.Click += new System.EventHandler(this.restauranteToolStripMenuItem_Click);
             // 
@@ -175,7 +177,7 @@
             // 
             this.hotelToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hotelToolStripMenuItem.Name = "hotelToolStripMenuItem";
-            this.hotelToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
+            this.hotelToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
             this.hotelToolStripMenuItem.Text = "Hotel";
             this.hotelToolStripMenuItem.Click += new System.EventHandler(this.hotelToolStripMenuItem_Click);
             // 
@@ -183,7 +185,7 @@
             // 
             this.departamentoToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.departamentoToolStripMenuItem.Name = "departamentoToolStripMenuItem";
-            this.departamentoToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
+            this.departamentoToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
             this.departamentoToolStripMenuItem.Text = "Departamento";
             this.departamentoToolStripMenuItem.Click += new System.EventHandler(this.departamentoToolStripMenuItem_Click);
             // 
@@ -191,16 +193,30 @@
             // 
             this.clienteToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
-            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
             this.clienteToolStripMenuItem.Text = "Cliente";
             this.clienteToolStripMenuItem.Click += new System.EventHandler(this.clienteToolStripMenuItem_Click);
             // 
             // bonosYDescuentosToolStripMenuItem
             // 
             this.bonosYDescuentosToolStripMenuItem.Name = "bonosYDescuentosToolStripMenuItem";
-            this.bonosYDescuentosToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
+            this.bonosYDescuentosToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
             this.bonosYDescuentosToolStripMenuItem.Text = "Bonos Y Descuentos";
             this.bonosYDescuentosToolStripMenuItem.Click += new System.EventHandler(this.bonosYDescuentosToolStripMenuItem_Click);
+            // 
+            // guiaTuristicoToolStripMenuItem
+            // 
+            this.guiaTuristicoToolStripMenuItem.Name = "guiaTuristicoToolStripMenuItem";
+            this.guiaTuristicoToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.guiaTuristicoToolStripMenuItem.Text = "Guia Turistico";
+            this.guiaTuristicoToolStripMenuItem.Click += new System.EventHandler(this.guiaTuristicoToolStripMenuItem_Click);
+            // 
+            // empleadoToolStripMenuItem
+            // 
+            this.empleadoToolStripMenuItem.Name = "empleadoToolStripMenuItem";
+            this.empleadoToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.empleadoToolStripMenuItem.Text = "Empleado";
+            this.empleadoToolStripMenuItem.Click += new System.EventHandler(this.empleadoToolStripMenuItem_Click);
             // 
             // contabilidadToolStripMenuItem
             // 
@@ -211,14 +227,14 @@
             this.contabilidadToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.contabilidadToolStripMenuItem.Name = "contabilidadToolStripMenuItem";
             this.contabilidadToolStripMenuItem.Padding = new System.Windows.Forms.Padding(10, 0, 4, 10);
-            this.contabilidadToolStripMenuItem.Size = new System.Drawing.Size(131, 34);
+            this.contabilidadToolStripMenuItem.Size = new System.Drawing.Size(116, 31);
             this.contabilidadToolStripMenuItem.Text = "Contabilidad";
             // 
             // nóminasToolStripMenuItem
             // 
             this.nóminasToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nóminasToolStripMenuItem.Name = "nóminasToolStripMenuItem";
-            this.nóminasToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.nóminasToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.nóminasToolStripMenuItem.Text = "Nóminas";
             this.nóminasToolStripMenuItem.Click += new System.EventHandler(this.nóminasToolStripMenuItem_Click);
             // 
@@ -226,7 +242,7 @@
             // 
             this.cerrarSesiónToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cerrarSesiónToolStripMenuItem.Name = "cerrarSesiónToolStripMenuItem";
-            this.cerrarSesiónToolStripMenuItem.Size = new System.Drawing.Size(138, 34);
+            this.cerrarSesiónToolStripMenuItem.Size = new System.Drawing.Size(120, 31);
             this.cerrarSesiónToolStripMenuItem.Text = "Cerrar Sesión";
             this.cerrarSesiónToolStripMenuItem.Click += new System.EventHandler(this.cerrarSesiónToolStripMenuItem_Click);
             // 
@@ -235,35 +251,27 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 708);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 711);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1182, 25);
+            this.statusStrip1.Size = new System.Drawing.Size(1182, 22);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(59, 20);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(47, 17);
             this.toolStripStatusLabel1.Text = "Usuario";
             // 
-            // guiaTuristicoToolStripMenuItem
+            // menuToolStripMenuItem
             // 
-            this.guiaTuristicoToolStripMenuItem.Name = "guiaTuristicoToolStripMenuItem";
-            this.guiaTuristicoToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
-            this.guiaTuristicoToolStripMenuItem.Text = "Guia Turistico";
-            this.guiaTuristicoToolStripMenuItem.Click += new System.EventHandler(this.guiaTuristicoToolStripMenuItem_Click);
-            // 
-            // empleadoToolStripMenuItem
-            // 
-            this.empleadoToolStripMenuItem.Name = "empleadoToolStripMenuItem";
-            this.empleadoToolStripMenuItem.Size = new System.Drawing.Size(260, 28);
-            this.empleadoToolStripMenuItem.Text = "Empleado";
-            this.empleadoToolStripMenuItem.Click += new System.EventHandler(this.empleadoToolStripMenuItem_Click);
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.menuToolStripMenuItem.Text = "Menu";
             // 
             // Frm_mdi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CadetBlue;
             this.ClientSize = new System.Drawing.Size(1182, 733);
@@ -312,5 +320,6 @@
         private System.Windows.Forms.ToolStripMenuItem bonosYDescuentosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem guiaTuristicoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem empleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
     }
 }
