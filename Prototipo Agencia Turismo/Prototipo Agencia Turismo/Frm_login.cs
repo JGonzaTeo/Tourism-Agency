ebc9357e5438f813866c9c94b69d6d5dc2634655
Edson Juarez <ejuarezm10@miumg.edu.gt>
2019-08-01 22:44:07 -0600

    Correcciones en el formulario de login

--- Prototipo Agencia Turismo/Prototipo Agencia Turismo/Frm_login.cs
+++ Prototipo Agencia Turismo/Prototipo Agencia Turismo/Frm_login.cs
@@ -21,6 +21,13 @@ namespace Prototipo_Agencia_Turismo
 {
     public partial class Frm_login : Form
     {
+        string contrasenaDesencriptada = " ";
+        int idUsuario = 0;
+        string nombreUsuario = " ";
+        string tipoPerfil = " ";
+        string actualizarCampo = " ";
+        bool datosIncorrectos = false;
+
         public Frm_login()
         {
             InitializeComponent();
@@ -28,63 +35,64 @@ namespace Prototipo_Agencia_Turismo
 
         private void IniciarSesion()
         {
+            contrasenaDesencriptada = " ";
+            idUsuario = 0;
+            nombreUsuario = " ";
+            tipoPerfil = " ";
+            actualizarCampo = " ";
+            DateTime fecha_ingreso = DateTime.Now;
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip.ToString();
+                }
+            }
+
             try
             {
-                string contrasenaDesencriptada = " ";
-                int idUsuario = 0;
-                string nombreUsuario = " ";
-                string tipoPerfil = " ";
-                DateTime fecha_ingreso = DateTime.Now;
-
-                string actualizarCampo = " ";
-
-                IPHostEntry host;
-                string localIP = "?";
-                host = Dns.GetHostEntry(Dns.GetHostName());
-
-                foreach (IPAddress ip in host.AddressList)
-                {
-                    if (ip.AddressFamily.ToString() == "InterNetwork")
-                    {
-                        localIP = ip.ToString();
-                    }
-                }
-
+                //Ejecución de consulta para buscar el usuario
                 string consultaUsuario = string.Format("SELECT * FROM tbl_usuario WHERE estado = 1 AND logeado = 0;");
                 OdbcCommand comm = new OdbcCommand(consultaUsuario, Conexion.nuevaConexion());
                 OdbcDataReader mostrarUsuarios = comm.ExecuteReader();
 
                 while (mostrarUsuarios.Read())
                 {
+                    //guarda la contraseña del usuario desencriptada 
                     contrasenaDesencriptada = Seguridad_Login.DesEncriptar(mostrarUsuarios.GetString(2));
-                    Console.WriteLine("CONTRASENA DESENCRIPTADA: " +contrasenaDesencriptada);
-                    if ((Txt_usuario.Text == mostrarUsuarios.GetString(1)) && (Txt_contrasena.Text == contrasenaDesencriptada))
+                    nombreUsuario = mostrarUsuarios.GetString(1);
+                    //si los campos escritos en el Formulario son iguales a los del registro de la BD se permite el logeo
+                    if (Txt_usuario.Text != mostrarUsuarios.GetString(1) || Txt_contrasena.Text != contrasenaDesencriptada)
+                    {
+                        //MessageBox.Show("DATOS INCORRECTOS");
+                        Txt_usuario.Focus();
+                        datosIncorrectos = true;
+                    }
+                    else
                     {
                         idUsuario = mostrarUsuarios.GetInt32(0);
                         nombreUsuario = mostrarUsuarios.GetString(1);
                         tipoPerfil = mostrarUsuarios.GetString(3);
+
                         MessageBox.Show("INICIANDO SESIÓN");
                         Frm_mdi mdiMenu = new Frm_mdi(idUsuario, nombreUsuario, tipoPerfil);
                         this.Hide();
                         mdiMenu.Show();
+
                         Console.WriteLine("Inicio de sesión");
-                        
+                        //Consulta para verificar que el usuario solo pueda logearse al sistema 1 vez
                         actualizarCampo = "UPDATE tbl_usuario SET logeado = '1' WHERE Pk_idUsuario= " + idUsuario + " AND Fk_idPerfil= '" + tipoPerfil + "'";
                         OdbcCommand commAct = new OdbcCommand(actualizarCampo, Conexion.nuevaConexion());
                         commAct.ExecuteNonQuery();
-
+                        datosIncorrectos = false;
                         break;
                     }
-                    else
-                    {
-                        Console.Write("DATOS INCORRECTOS");
-                        Txt_usuario.Focus();
-                    }
                 }
-                Console.WriteLine("ID DE USUARIO : " + mostrarUsuarios.GetString(0));
-               
                 
-
                 OdbcCommand commBitacora = new OdbcCommand("{call SP_InsertarBitacora(?,?,?,?,?)}", Conexion.nuevaConexion());
                 commBitacora.CommandType = CommandType.StoredProcedure;
                 commBitacora.Parameters.Add("ope", OdbcType.Text).Value = "INICION DE SESIÓN";
@@ -94,11 +102,15 @@ namespace Prototipo_Agencia_Turismo
                 commBitacora.Parameters.Add("dirIp", OdbcType.Text).Value = localIP;
                 commBitacora.ExecuteNonQuery();
 
-
+                if(datosIncorrectos == true)
+                {
+                    MessageBox.Show("DATOS INCORRECTOS");
+                }
             }
             catch(Exception err)
             {
                 Console.Write("Error: " + err.Message);
+                Console.Write("DATOS INCORRECTOS");
             }
 
         }
