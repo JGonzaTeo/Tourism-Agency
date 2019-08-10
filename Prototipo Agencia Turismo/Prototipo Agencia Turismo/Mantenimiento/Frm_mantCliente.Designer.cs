d716aa12ea350f52e753efd76a2ce4baaa61b6bc
Angel Solares <asolaresm3@miumg.edu.gt>
2019-08-10 17:20:11 -0600

    Reporte de Cliente

--- Prototipo Agencia Turismo/Prototipo Agencia Turismo/Mantenimiento/Frm_mantCliente.Designer.cs
+++ Prototipo Agencia Turismo/Prototipo Agencia Turismo/Mantenimiento/Frm_mantCliente.Designer.cs
@@ -58,7 +58,6 @@
             this.Lbl_Telres = new System.Windows.Forms.Label();
             this.Txt_CorreoAlt = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.Btn_generarReporte = new System.Windows.Forms.Button();
             this.Pnl_principal.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -119,7 +118,7 @@
             this.Lbl_titulo.Location = new System.Drawing.Point(34, 16);
             this.Lbl_titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_titulo.Name = "Lbl_titulo";
-            this.Lbl_titulo.Size = new System.Drawing.Size(98, 20);
+            this.Lbl_titulo.Size = new System.Drawing.Size(82, 17);
             this.Lbl_titulo.TabIndex = 1;
             this.Lbl_titulo.Text = "CLIENTES";
             // 
@@ -248,7 +247,7 @@
             this.Txt_dpi.Enabled = false;
             this.Txt_dpi.Location = new System.Drawing.Point(414, 414);
             this.Txt_dpi.Name = "Txt_dpi";
-            this.Txt_dpi.Size = new System.Drawing.Size(138, 29);
+            this.Txt_dpi.Size = new System.Drawing.Size(138, 24);
             this.Txt_dpi.TabIndex = 48;
             // 
             // Lbl_dpi
@@ -256,7 +255,7 @@
             this.Lbl_dpi.AutoSize = true;
             this.Lbl_dpi.Location = new System.Drawing.Point(187, 417);
             this.Lbl_dpi.Name = "Lbl_dpi";
-            this.Lbl_dpi.Size = new System.Drawing.Size(249, 24);
+            this.Lbl_dpi.Size = new System.Drawing.Size(195, 18);
             this.Lbl_dpi.TabIndex = 47;
             this.Lbl_dpi.Text = "Documento de Identificacion";
             // 
@@ -265,7 +264,7 @@
             this.Txt_telefonoCel.Enabled = false;
             this.Txt_telefonoCel.Location = new System.Drawing.Point(362, 281);
             this.Txt_telefonoCel.Name = "Txt_telefonoCel";
-            this.Txt_telefonoCel.Size = new System.Drawing.Size(190, 29);
+            this.Txt_telefonoCel.Size = new System.Drawing.Size(190, 24);
             this.Txt_telefonoCel.TabIndex = 46;
             this.Txt_telefonoCel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_telefono_KeyPress);
             // 
@@ -274,7 +273,7 @@
             this.Lbl_telefonoCel.AutoSize = true;
             this.Lbl_telefonoCel.Location = new System.Drawing.Point(187, 288);
             this.Lbl_telefonoCel.Name = "Lbl_telefonoCel";
-            this.Lbl_telefonoCel.Size = new System.Drawing.Size(149, 24);
+            this.Lbl_telefonoCel.Size = new System.Drawing.Size(116, 18);
             this.Lbl_telefonoCel.TabIndex = 45;
             this.Lbl_telefonoCel.Text = "Telefono Celular";
             // 
@@ -283,7 +282,7 @@
             this.Txt_Nombre.Enabled = false;
             this.Txt_Nombre.Location = new System.Drawing.Point(362, 219);
             this.Txt_Nombre.Name = "Txt_Nombre";
-            this.Txt_Nombre.Size = new System.Drawing.Size(190, 29);
+            this.Txt_Nombre.Size = new System.Drawing.Size(190, 24);
             this.Txt_Nombre.TabIndex = 44;
             // 
             // Lbl_nombreCliente
@@ -291,7 +290,7 @@
             this.Lbl_nombreCliente.AutoSize = true;
             this.Lbl_nombreCliente.Location = new System.Drawing.Point(187, 226);
             this.Lbl_nombreCliente.Name = "Lbl_nombreCliente";
-            this.Lbl_nombreCliente.Size = new System.Drawing.Size(79, 24);
+            this.Lbl_nombreCliente.Size = new System.Drawing.Size(62, 18);
             this.Lbl_nombreCliente.TabIndex = 43;
             this.Lbl_nombreCliente.Text = "Nombre";
             // 
@@ -300,7 +299,7 @@
             this.Txt_idCliente.Enabled = false;
             this.Txt_idCliente.Location = new System.Drawing.Point(362, 160);
             this.Txt_idCliente.Name = "Txt_idCliente";
-            this.Txt_idCliente.Size = new System.Drawing.Size(138, 29);
+            this.Txt_idCliente.Size = new System.Drawing.Size(138, 24);
             this.Txt_idCliente.TabIndex = 42;
             this.Txt_idCliente.Visible = false;
             // 
@@ -309,7 +308,7 @@
             this.Lbl_idTransporte.AutoSize = true;
             this.Lbl_idTransporte.Location = new System.Drawing.Point(187, 166);
             this.Lbl_idTransporte.Name = "Lbl_idTransporte";
-            this.Lbl_idTransporte.Size = new System.Drawing.Size(90, 24);
+            this.Lbl_idTransporte.Size = new System.Drawing.Size(71, 18);
             this.Lbl_idTransporte.TabIndex = 41;
             this.Lbl_idTransporte.Text = "ID Cliente";
             this.Lbl_idTransporte.Visible = false;
@@ -319,7 +318,7 @@
             this.Txt_nit.Enabled = false;
             this.Txt_nit.Location = new System.Drawing.Point(752, 411);
             this.Txt_nit.Name = "Txt_nit";
-            this.Txt_nit.Size = new System.Drawing.Size(138, 29);
+            this.Txt_nit.Size = new System.Drawing.Size(138, 24);
             this.Txt_nit.TabIndex = 60;
             // 
             // Lbl_nit
@@ -327,7 +326,7 @@
             this.Lbl_nit.AutoSize = true;
             this.Lbl_nit.Location = new System.Drawing.Point(577, 417);
             this.Lbl_nit.Name = "Lbl_nit";
-            this.Lbl_nit.Size = new System.Drawing.Size(40, 24);
+            this.Lbl_nit.Size = new System.Drawing.Size(31, 18);
             this.Lbl_nit.TabIndex = 59;
             this.Lbl_nit.Text = "NIT";
             // 
@@ -336,7 +335,7 @@
             this.Txt_correo.Enabled = false;
             this.Txt_correo.Location = new System.Drawing.Point(752, 281);
             this.Txt_correo.Name = "Txt_correo";
-            this.Txt_correo.Size = new System.Drawing.Size(190, 29);
+            this.Txt_correo.Size = new System.Drawing.Size(190, 24);
             this.Txt_correo.TabIndex = 58;
             // 
             // Lbl_correo
@@ -344,7 +343,7 @@
             this.Lbl_correo.AutoSize = true;
             this.Lbl_correo.Location = new System.Drawing.Point(577, 288);
             this.Lbl_correo.Name = "Lbl_correo";
-            this.Lbl_correo.Size = new System.Drawing.Size(68, 24);
+            this.Lbl_correo.Size = new System.Drawing.Size(55, 18);
             this.Lbl_correo.TabIndex = 57;
             this.Lbl_correo.Text = "Correo";
             // 
@@ -353,7 +352,7 @@
             this.Txt_Apellido.Enabled = false;
             this.Txt_Apellido.Location = new System.Drawing.Point(752, 219);
             this.Txt_Apellido.Name = "Txt_Apellido";
-            this.Txt_Apellido.Size = new System.Drawing.Size(190, 29);
+            this.Txt_Apellido.Size = new System.Drawing.Size(190, 24);
             this.Txt_Apellido.TabIndex = 56;
             // 
             // Lbl_Apellido
@@ -361,7 +360,7 @@
             this.Lbl_Apellido.AutoSize = true;
             this.Lbl_Apellido.Location = new System.Drawing.Point(577, 226);
             this.Lbl_Apellido.Name = "Lbl_Apellido";
-            this.Lbl_Apellido.Size = new System.Drawing.Size(79, 24);
+            this.Lbl_Apellido.Size = new System.Drawing.Size(59, 18);
             this.Lbl_Apellido.TabIndex = 55;
             this.Lbl_Apellido.Text = "Apellido";
             // 
@@ -370,7 +369,7 @@
             this.Txt_TelefonoRes.Enabled = false;
             this.Txt_TelefonoRes.Location = new System.Drawing.Point(362, 347);
             this.Txt_TelefonoRes.Name = "Txt_TelefonoRes";
-            this.Txt_TelefonoRes.Size = new System.Drawing.Size(190, 29);
+            this.Txt_TelefonoRes.Size = new System.Drawing.Size(190, 24);
             this.Txt_TelefonoRes.TabIndex = 62;
             this.Txt_TelefonoRes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_TelefonoRes_KeyPress);
             // 
@@ -379,7 +378,7 @@
             this.Lbl_Telres.AutoSize = true;
             this.Lbl_Telres.Location = new System.Drawing.Point(187, 354);
             this.Lbl_Telres.Name = "Lbl_Telres";
-            this.Lbl_Telres.Size = new System.Drawing.Size(188, 24);
+            this.Lbl_Telres.Size = new System.Drawing.Size(146, 18);
             this.Lbl_Telres.TabIndex = 61;
             this.Lbl_Telres.Text = "Telefono Residencial";
             // 
@@ -388,7 +387,7 @@
             this.Txt_CorreoAlt.Enabled = false;
             this.Txt_CorreoAlt.Location = new System.Drawing.Point(752, 347);
             this.Txt_CorreoAlt.Name = "Txt_CorreoAlt";
-            this.Txt_CorreoAlt.Size = new System.Drawing.Size(190, 29);
+            this.Txt_CorreoAlt.Size = new System.Drawing.Size(190, 24);
             this.Txt_CorreoAlt.TabIndex = 64;
             // 
             // label1
@@ -396,35 +395,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(577, 354);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 24);
+            this.label1.Size = new System.Drawing.Size(127, 18);
             this.label1.TabIndex = 63;
             this.label1.Text = "Correo Alternativo";
             // 
-            // Btn_generarReporte
-            // 
-            this.Btn_generarReporte.Enabled = false;
-            this.Btn_generarReporte.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.Btn_generarReporte.FlatAppearance.BorderSize = 3;
-            this.Btn_generarReporte.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.Btn_generarReporte.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaGreen;
-            this.Btn_generarReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_generarReporte.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_generarReporte.Image = ((System.Drawing.Image)(resources.GetObject("Btn_generarReporte.Image")));
-            this.Btn_generarReporte.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_generarReporte.Location = new System.Drawing.Point(12, 47);
-            this.Btn_generarReporte.Name = "Btn_generarReporte";
-            this.Btn_generarReporte.Size = new System.Drawing.Size(189, 41);
-            this.Btn_generarReporte.TabIndex = 65;
-            this.Btn_generarReporte.Text = "Generar reporte";
-            this.Btn_generarReporte.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Btn_generarReporte.UseVisualStyleBackColor = true;
-            // 
             // Frm_mantCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1180, 640);
-            this.Controls.Add(this.Btn_generarReporte);
             this.Controls.Add(this.Txt_CorreoAlt);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Txt_TelefonoRes);
@@ -495,6 +474,5 @@
         private System.Windows.Forms.TextBox Txt_CorreoAlt;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Btn_ayuda;
-        private System.Windows.Forms.Button Btn_generarReporte;
     }
 }
---
+++ Prototipo Agencia Turismo/Prototipo Agencia Turismo/Reportes/Frm_reporteCliente.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ -----------------------------------------------------
+            AUTOR: Angel Solares
+  -----------------------------------------------------
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo_Agencia_Turismo.Reportes
+{
+    public partial class Frm_reporteCliente : Form
+    {
+        public Frm_reporteCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
